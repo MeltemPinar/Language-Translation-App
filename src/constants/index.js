cc484e4cc08a764e1e0c8ea2c14b3c83e3a82181
MeltemPinar <nemmeltem@gmail.com>
2024-05-20 23:25:40 +0300

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,7 @@
+export const languageOptions = {
+  url: "https://text-translator2.p.rapidapi.com/getLanguages",
+  headers: {
+    "X-RapidAPI-Key": "951e708bcamsh8e31c23d62696a8p102b25jsn096709fcd1ae",
+    "X-RapidAPI-Host": "text-translator2.p.rapidapi.com",
+  },
+};
